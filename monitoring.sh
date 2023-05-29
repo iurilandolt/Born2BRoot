404f1417a6d60a884eeb1eb712c51134f18f89ff
rlandolt <rlandolt@c2r5s3.42lisboa.com>
2023-05-29 19:33:44 +0100

    fixes do monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -10,23 +10,23 @@ ramp=$(free -m | grep Mem | awk '{printf("%d%%"), $3 / $2 * 100}')
 diskt=$(df -m --total | grep total | awk '{print $2}')
 disku=$(df -m --total | grep total | awk '{print $3}')
 diskp=$(df -m --total | grep total | awk '{print $5}')
-cpup=$(top -bn1 | awk '/^%Cpu/ {print (100 - $8) "%"}')
-boot=$(who -b | awk '{print ($3 " @ " $4)}')
+cpup=$(top -bn1| grep '%Cpu(s)' | awk '{printf("%d%%", $2 + $4)}')
+rboot=$(last | grep 'still running' -m 1 | awk '{print $5" "$6" "$7" @ "$8}')
 lvm=$(if lsblk | grep -q lvm; then echo yes; else echo no; fi)
-tcp=$(ss -t | grep '^ESTAB' | wc -l)
-users=$(who | wc -l)
+tcp=$(ss | grep 'tcp' | wc -l)
+users=$(users | wc -w)
 ip=$(hostname -I)
 mac=$(ip link | grep link/ether | awk '{print $2}')
-sudo=$(grep COMMAND /var/log/sudo/sudo.log | wc -l)
+sudo=$(grep 'COMMAND=' /var/log/sudo/sudo.log | wc -l)
 
 wall "
 	Architecture	: $arch
 	PCPUs			: $pcpu
 	VCPUs			: $tcpu
-	Memory Usage	: $ramu / $ramt"mb" ($ramp)
-	Disk Usage		: $disku / $diskt"mb" ($diskp)
+	Memory Usage	: ($ramp) $ramu"mb"/$ramt"mb"
+	Disk Usage		: ($diskp) $disku"mb"/$diskt"mb"
 	CPU Load		: $cpup
-	Last Boot		: $boot
+	Last Reboot		: $rboot
 	LVM use			: $lvm
 	TCP Connections	: $tcp established
 	Users logged	: $users
